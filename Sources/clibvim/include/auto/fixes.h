65fee483c290249276cec94a43229609a3db3fb3
Kaï-Zen Fœnyx Krysies Berg-Šæmañn <k@izen.one>
2024-03-18 13:35:58 -0400

    Get SPM to compile clibvim

---
+++ Sources/clibvim/include/auto/fixes.h
@@ -0,0 +1,19 @@
+#ifndef FIXES_H_
+#define FIXES_H_
+
+#define buf_valid buf_valid_
+#define entry entry_
+
+#ifndef HAVE_CONFIG_H
+#define HAVE_CONFIG_H
+#endif
+
+#ifndef MACOS_X
+#define MACOS_X
+#endif
+
+#ifndef MACOS_X_DARWIN
+#define MACOS_X_DARWIN
+#endif
+
+#endif // FIXES_H_
